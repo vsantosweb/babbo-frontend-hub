c26e467078e595d252d10d4c5707f34d2383431d
Vitor Santos <vsantosweb@github.com>
2024-04-03 12:37:26 -0300

    ads config

--- lib/components/Google/GoogleAdSense/index.tsx
+++ lib/components/Google/GoogleAdSense/index.tsx
@@ -13,22 +13,19 @@ export const GoogleAdSense: React.FC<{ adClient: string; adSlot: string }> = ({
   adClient,
   adSlot,
 }) => {
-  
   useEffect(() => {
-    var ads = document.getElementsByClassName('adsbygoogle').length;
-    for (var i = 0; i < ads; i++) {
-      try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-      } catch (e) {}
+    if (window.adsbygoogle && window.adsbygoogle.push) {
+      window.adsbygoogle.push({});
     }
   }, []);
 
   return (
     <ins className="adsbygoogle"
-     style={{display:'block'}}
-     data-ad-client="ca-pub-8530046753205274"
-     data-ad-slot="6943411120"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-8530046753205274"
+      data-ad-slot="6943411120"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
   );
+
 };
