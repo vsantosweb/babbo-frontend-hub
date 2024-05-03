9b8f2f7bf8a729bee0a01811ed992fd6012ed93f
Vitor Santos <vsantosweb@github.com>
2024-05-03 13:10:35 -0300

    update pack

--- apps/manager/next.config.js
+++ apps/manager/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-  output: 'export',
+  // output: 'export',
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx'],
 };
 
